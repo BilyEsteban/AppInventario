74387687f495a9580ec1fc6475ca04570a074b30
Bily-Ubri <billsesteban11@hotmail.com>
2026-06-11 12:50:29 -0400

    Add project files.

---
+++ AppInventario/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppInventario.ViewsModels;
+
+namespace AppInventario
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(
+                nameof(ProductFormPage),
+                typeof(ProductFormPage));
+
+            Routing.RegisterRoute(
+                nameof(LoginPage),
+                typeof(LoginPage));
+        }
+
+    }
+}
---
+++ AppInventario/ViewModels/LoginPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppInventario.ViewsModels
+{
+    internal class LoginPage
+    {
+    }
+}
